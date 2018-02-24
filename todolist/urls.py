472fd6e6c70d1dc714bd48233d085caa764fc4a6
Alladdin <alexandriksasha@mail.ru>
2018-02-24 16:51:47 -0500

    Signup

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,5 +7,6 @@ urlpatterns =  [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def post_list(request):
     if request.method == "POST":
@@ -45,6 +47,19 @@ def post_delete(request, pk):
     post.delete()
     return redirect('post_list')
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 
